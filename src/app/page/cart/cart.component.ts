d4cd231e6a23f11757db09788b03e327bb599bd8
Zhao Hsin Rui (Ray) <bill56523@gmail.com>
2024-03-02 17:14:46 +0800

    使用表單進行使用者輸入

--- src/app/page/cart/cart.component.ts
+++ src/app/page/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../service/cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,8 +11,22 @@ export class CartComponent {
 
   // --- Params
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name:'',
+    address:''
+  })
 
   // --- Constructors
-  constructor(private cartService: CartService){}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ){ }
+
+  // --- Functions
+  onSubmit(): void{
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
 }
